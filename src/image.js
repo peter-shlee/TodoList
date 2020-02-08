88b2a60f4480e0153bde6de593f4448afbc18b8e
shlee4290 <47245682+shlee4290@users.noreply.github.com>
2020-02-08 21:42:11 +0900

    Update image.js

--- src/image.js
+++ src/image.js
@@ -6,13 +6,12 @@ let image;
 function showImage(imageNumber) {
   image = new Image();
   image.addEventListener("loadend", onLoadEnd);
-  body.appendChild(image);
   image.src = `images/${imageNumber + 1}.jpg`;
-  //body.prepend(image);
 }
 
 function onLoadEnd() {
   image.classList.add("bgImage");
+  body.prepend(image);
 }
 
 function generateRandomNumber() {
