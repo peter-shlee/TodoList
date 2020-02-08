3a9e8f3856be0a87c46e701e7582eab10ddddb82
shlee4290 <47245682+shlee4290@users.noreply.github.com>
2020-02-08 21:30:07 +0900

    Add files via upload

--- src/image.js
+++ src/image.js
@@ -1,12 +1,17 @@
 const body = document.querySelector("body");
 
 const IMAGE_COUNT = 9;
+let image;
 
 function showImage(imageNumber) {
-  const image = new Image();
+  image = new Image();
   image.src = `images/${imageNumber + 1}.jpg`;
-  image.classList.add("bgImage");
+  image.addEventListener("loadend", onLoadEnd);
+}
+
+function onLoadEnd() {
   body.prepend(image);
+  image.classList.add("bgImage");
 }
 
 function generateRandomNumber() {
